ec6fd0e9f6de99a8e3eda133677d488130c44a2d
Alina Aleks <toalinaaleks@gmail.com>
2026-07-08 15:46:54 +0300

    Reorganize folders

--- src/load_raw.py
+++ src/load_raw.py
@@ -1,20 +1,17 @@
 import pandas as pd
-from db import get_engine, ensure_schema
+from db import get_engine
 
 engine = get_engine()
-ensure_schema(engine)
 
-def load_csv(file_path, table_name):
-    df = pd.read_csv(file_path, sep=';')
-    df.to_sql(table_name, engine, schema="raw", if_exists="replace", index=False)
-    print(f"Loaded {table_name}")
+df = pd.read_csv(
+    "data/raw/payments.csv",
+    sep=";"
+)
 
-if __name__ == "__main__":
-    load_csv("data/raw/projects.csv", "projects")
-    load_csv("data/raw/vessels.csv", "vessels")
-    load_csv("data/raw/counterparts.csv", "counterparts")
-    load_csv("data/raw/contract_groups.csv", "contract_groups")
-    load_csv("data/raw/contracts.csv", "contracts")
-    load_csv("data/raw/vessel_allocation.csv", "vessel_allocation")
-    load_csv("data/raw/budget_categories.csv", "budget_categories")
-    load_csv("data/raw/payments.csv", "payments")
+df.to_sql(
+    "payments",
+    engine,
+    schema="raw",
+    if_exists="replace",
+    index=False
+)
