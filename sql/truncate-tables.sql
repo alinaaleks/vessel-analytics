6195ebebd16d343c8b78fca6eafa1e011ba57ae6
Alina Aleks <toalinaaleks@gmail.com>
2026-07-09 16:27:53 +0300

    Add truncate ALL tables for reloading ALL into DB

---
+++ sql/truncate-tables.sql
@@ -0,0 +1,10 @@
+TRUNCATE TABLE
+    raw.payments,
+    raw.vessel_allocation,
+    raw.contracts,
+    raw.contract_groups,
+    raw.vessels,
+    raw.contract_types,
+    raw.counterparts,
+    raw.projects
+CASCADE;
